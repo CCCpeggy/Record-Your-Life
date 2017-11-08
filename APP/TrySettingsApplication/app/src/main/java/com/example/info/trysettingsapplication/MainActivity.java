7260de8a97a4ba6a58a94e2d405f4d5bf1630fb5
RecordYourLife <32979547+RecordYourLife@users.noreply.github.com>
2017-11-08 17:42:00 +0800

    deleted settingsActivity
    deleted settingsActivity

--- APP/TrySettingsApplication/app/src/main/java/com/example/info/trysettingsapplication/MainActivity.java
+++ APP/TrySettingsApplication/app/src/main/java/com/example/info/trysettingsapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.info.trysettingsapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
---
+++ APP/TrySettingsApplication/app/src/main/java/com/example/info/trysettingsapplication/TableActivity.java
@@ -0,0 +1,17 @@
+package com.example.info.trysettingsapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by info on 2017/11/8.
+ */
+
+public class TableActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablesettings);
+    }
+
+}
