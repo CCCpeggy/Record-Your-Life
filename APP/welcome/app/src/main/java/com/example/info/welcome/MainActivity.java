4219db0ad39e75b31c2f5b5768ea293a68634b59
woodwindflute <33533969+woodwindflute@users.noreply.github.com>
2017-12-05 19:18:52 +0800

    歡迎頁

--- APP/welcome/app/src/main/java/com/example/info/welcome/MainActivity.java
+++ APP/welcome/app/src/main/java/com/example/info/welcome/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.info.welcome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class welcome extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
     }
 }
